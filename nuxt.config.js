abe48911f924e300714cdf2a84e5f2b6711fe1d0
ortamfag <a_zhidov@outlook.com>
2022-02-05 14:06:48 +0300

    ADD: fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,12 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
+
+    target: 'static',
+    router: {
+      base: '/web_exam/'
+    },
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
